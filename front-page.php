c740bb2d6ed1381f1236fd91a8629cde9536725c
nickty <nickty.86@gmail.com>
2022-11-26 09:13:03 +0600

    flex applied to all

--- front-page.php
+++ front-page.php
@@ -18,45 +18,40 @@
 <div class="container">
   <div class="row">
     <div class="col-sm">
-    <div class="hero-tesxt">
-    <?php if(get_field('bodytext')): ?>
-       
+        <div class="body-text">
+            <?php if(get_field('bodytext')): ?>
+                <p>    <?php the_field('bodytext'); ?> </p>
                 
-                    <?php the_field('bodytext'); ?>
-                
-                
-       
-    <?php endif; ?>
+           <?php endif; ?>
+        </div>
     </div>
-    </div>
-    
+   
   </div>
 </div>
 
 <section class="teaser-section">
     <div class="container">
-        <div class="teaser">
-        <?php if(get_field('title')): ?>
+        
+        <div class="row teaser-holder">
+            <div class="col-sm-4">
+                <div class="teaser">
+                    <?php if(get_field('title')): ?>
        
-       <div class="text-center">
-           <?php the_field('title'); ?>
-       </div>
-        <?php endif; ?>
-        </div>
-        <div class="row">
-            <div class="col">
+                        <div>
+                            <h2><?php the_field('title'); ?></h2>
+                        </div>
+                    <?php endif; ?>
+                </div>
                 <img src="<?php the_field('mobile-image'); ?>" alt="" srcset="">
             </div>
-            <div class="col">
+            <div class="col-sm-8">
                 <?php the_field('teaser-body'); ?>
-            </div>
-            <div class="text-center">
-                <?php the_field('teaser-body'); ?>
-            </div>
-            <div class="btn">
-                <button><?php the_field('btn-text'); ?></button>
-            </div>
             
+            
+                <div>
+                    <button  class="btn mt-5"><?php the_field('btn-text'); ?></button>
+                </div>
+            </div>
         </div>
     </div>
 </section>
@@ -64,10 +59,10 @@
 <section class="three-columns">
     <div class="container">
         <div class="header-3columns">
-        <?php the_field('header'); ?>
+        <h2 class="mb-3"><?php the_field('header'); ?></h2>
        
       <div class="row">
-            <div class="col">
+            <div class="col-sm-4">
                 <img src="<?php the_field('c1-image'); ?>" alt="" srcset="">
                 <h2>
                 <?php the_field('c1-title'); ?>
@@ -75,9 +70,9 @@
                 <p>
                 <?php the_field('c1-content') ?>
                 </p>
-                <button><?php the_field('c1-btn'); ?></button>
+                <button class="btn"><?php the_field('c1-btn'); ?></button>
             </div>
-            <div class="col">
+            <div class="col-sm-4">
                 <img src="<?php the_field('c2-image'); ?>" alt="" srcset="">
                 <h2>
                 <?php the_field('c2-title'); ?>
@@ -85,9 +80,9 @@
                 <p>
                 <?php the_field('c2-content') ?>
                 </p>
-                <button><?php the_field('c2-btn'); ?></button>
+                <button  class="btn"><?php the_field('c2-btn'); ?></button>
             </div>
-            <div class="col">
+            <div class="col-sm-4">
                 <img src="<?php the_field('c3-image'); ?>" alt="" srcset="">
                 <h2>
                 <?php the_field('c3-title'); ?>
@@ -95,7 +90,7 @@
                 <p>
                 <?php the_field('c3-content') ?>
                 </p>
-                <button><?php the_field('c3-btn'); ?></button>
+                <button  class="btn"><?php the_field('c3-btn'); ?></button>
             </div>
        </div>  
         
@@ -106,18 +101,18 @@
 <section class="freq">
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-sm-4">
                 <img src="<?php the_field('frequenzumrichter-image'); ?>" alt="" srcset="">
                 
             </div>
-            <div class="col">
-                
+            <div class="col-sm-8 fleq-inner-flex">
+                <h2>
                 <?php the_field('frequenzumrichter-title'); ?>
                 </h2>
                 <p>
                 <?php the_field('frequenzumrichter-body') ?>
                 </p>
-                <button><?php the_field('frequenzumrichter-btn'); ?></button>
+                <button  class="btn"><?php the_field('frequenzumrichter-btn'); ?></button>
             </div>
         </div>    
     </div>
@@ -126,16 +121,16 @@
 <section class="geba">
     <div class="container">
         <div class="row">
-            <div class="col">
-                
+            <div class="col-sm-8 fleq-inner-flex">
+                <h2>
                 <?php the_field('gebaudeautomation-title'); ?>
                 </h2>
                 <p>
                 <?php the_field('gebaudeautomation-body') ?>
                 </p>
-                <button><?php the_field('gebaudeautomation-btn'); ?></button>
+                <button  class="btn"><?php the_field('gebaudeautomation-btn'); ?></button>
             </div>
-            <div class="col">
+            <div class="col-sm-4">
                 <img src="<?php the_field('gebäudeautomation-image'); ?>" alt="" srcset="">
                 
             </div>
@@ -146,18 +141,18 @@
 <section class="motoren">
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-sm-4">
                 <img src="<?php the_field('motoren-image'); ?>" alt="" srcset="">
                 
             </div>
-            <div class="col">
-                
+            <div class="col-sm-8 fleq-inner-flex">
+                <h2>
                 <?php the_field('motoren-title'); ?>
                 </h2>
                 <p>
                 <?php the_field('motoren-body') ?>
                 </p>
-                <button><?php the_field('motoren-btn'); ?></button>
+                <button  class="btn"><?php the_field('motoren-btn'); ?></button>
             </div>
         </div>
     </div>
@@ -166,16 +161,16 @@
 <section class="digital">
     <div class="container">
         <div class="row">
-            <div class="col">
-                
+            <div class="col-sm-8 fleq-inner-flex">
+                <h2>
                 <?php the_field('digitalisierung-title'); ?>
                 </h2>
                 <p>
                 <?php the_field('digitalisierung-body') ?>
                 </p>
-                <button><?php the_field('digitalisierung-btn'); ?></button>
+                <button  class="btn"><?php the_field('digitalisierung-btn'); ?></button>
             </div>
-            <div class="col">
+            <div class="col-sm-4">
                 <img src="<?php the_field('digitalisierung-image'); ?>" alt="" srcset="">
                 
             </div>
